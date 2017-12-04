f15b4b84c34d2f29318d1f1da9724cb0f07c9d7b
Tony G Thomas <=>
2017-12-03 18:48:44 -0600

    Updated solution based on comments

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,13 +1,6 @@
 class Complement
-    TRANSCRIPTION_MAP = {
-        "G" => "C",
-        "C" => "G",
-        "T" => "A",
-        "A" => "U",
-    }
-
     def self.of_dna(strand)
-        return "" unless strand =~ /\A[GCTA]+\Z/
+        return "" unless /\A[GCTA]+\Z/.match(strand)
         strand.tr("GCTA", "CGAU")
     end
 end
